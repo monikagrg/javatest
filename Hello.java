ecfcf9e2a8c9badf43f74edef78ba772131afb92
monikagrg <162968966+monikagrg@users.noreply.github.com>
2024-03-11 15:21:27 +0545

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,17 @@
+mport java.util.Scanner;
 
+public class TestMonika {
+
+    public static void main(String[] args) {
+        int  [] a  = {2,3,4,5};
+        for(int i = 0; i<a.length;i++){
+            System.out.print(a[i]);
+        }
+
+        int b = 1;
+        if(b>1){
+            System.out.println("number is positive ");
+        }
+       else if (b <1){
+            System.out.println("number is negative");
+        }
